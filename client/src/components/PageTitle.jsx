7ee73050fedbf7c047c19034399d805a76d633d4
Hridoykhan4 <hridoykhan148385@gmail.com>
2026-05-07 22:54:09 +0600

    Navbar added

---
+++ client/src/components/PageTitle.jsx
@@ -0,0 +1,14 @@
+
+const PageTitle = ({ children }) => {
+    return (
+        <div className="bg-gradient-to-b lg:bg-gradient-to-l from-cyan-100 py-4">
+            <h2 className="w-11/12 text-center mx-auto  pl-2 border-primary lg:text-2xl ">
+                <span className="bg-gradient-to-l bg-clip-text text-transparent font-bold from-cyan-600 to-stone-300">
+                    {children}
+                </span>
+            </h2>
+        </div>
+    );
+};
+
+export default PageTitle;
